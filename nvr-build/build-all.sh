b834e1cb7e36db9643683453444753f3e99f05f5
Gonçalo Baltazar <me@goncalomb.com>
2021-10-30 23:04:07 +0100

    Redesign nvr-unleashed build process

--- nvr-build/build-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-cd -- "$(dirname -- "$0")"
-
-./build-bin.sh
-./build-modules.sh
---
+++ nvr-unleashed/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+cd -- "$(dirname -- "$0")"
+
+mkdir -p build build/bin build/modules
+
+# copy from src
+cp -a src/autoexec.sh build/
+
+# build binaries and modules
+build_env() {
+    docker build env-$1 -t nvr-unleashed-env-$1
+    docker run --rm -it -v "$(pwd)/build/$1:/work/$1" nvr-unleashed-env-$1:latest
+}
+
+build_env bin
+build_env modules
+
+echo
+echo "all binaries and modules"
+echo
+
+file build/bin/* build/modules/*
+
+echo
+echo "build done"
+echo "now copy the contents of the build folder pen drive for the nvr"
