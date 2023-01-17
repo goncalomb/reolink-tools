de0070b9074d860649e912f158c14b458d18b930
Gonçalo Baltazar <me@goncalomb.com>
2023-01-17 22:50:09 +0000

    Use submodules for nvr-unleashed bin dependencies
    Using older versions of nmap and iproute2 because of the current build
    environment is not compatible with newer versions. FIXME.

--- nvr-unleashed/env-bin/build.sh
+++ nvr-unleashed/env-bin/build.sh
@@ -7,67 +7,60 @@ mkdir -p bin
 
 if [ ! -f "bin/strace" ]; then (
     # strace
-    git clone --depth=1 https://github.com/strace/strace.git
-    cd strace
+    cd lib/strace
     # build
     export LDFLAGS="-static"
     ./bootstrap
     ./configure --host=arm
     make -j8
     # copy binaries
-    cp -at ../bin src/strace
+    cp -at ../../bin src/strace
 ) fi
 
 if [ ! -f "bin/nmap" ]; then (
     # nmap
-    git clone --depth=1 https://github.com/nmap/nmap.git
-    cd nmap
+    cd lib/nmap
     # build
     export LDFLAGS="-static"
     ./configure --host=arm
     make -j8
     # copy binaries
-    cp -at ../bin nmap ncat/ncat nping/nping
+    cp -at ../../bin nmap ncat/ncat nping/nping
 ) fi
 
 if [ ! -f "bin/arp-scan" ]; then (
     # libpcap
     (
-        git clone --depth=1 https://github.com/the-tcpdump-group/libpcap.git
-        cd libpcap
+        cd lib/libpcap
         ./configure --host=arm-linux
         make -j8
     )
     # arp-scan
-    git clone --depth=1 https://github.com/royhills/arp-scan.git
-    cd arp-scan
+    cd lib/arp-scan
     # build
-    export CFLAGS="-I/work/libpcap"
-    export LDFLAGS="-L/work/libpcap -static"
+    export CFLAGS="-I/work/lib/libpcap"
+    export LDFLAGS="-L/work/lib/libpcap -static"
     autoreconf --install
     ./configure --host=arm-linux
     make -j8
     # copy binaries
-    cp -at ../bin arp-scan
+    cp -at ../../bin arp-scan
 ) fi
 
 if [ ! -f "bin/ip" ]; then (
     # iproute2
-    git clone https://github.com/shemminger/iproute2.git
-    cd iproute2
-    git checkout 047e9ae516b5427158c0653f15c9c20d3965e4e5 # XXX: lock version, newer versions fail, fix this
+    cd lib/iproute2
     # build
     export LDFLAGS="-static"
     ./configure
     make -j8
     # copy binaries
-    cp -at ../bin ip/ip bridge/bridge
+    cp -at ../../bin ip/ip bridge/bridge
 ) fi
 
 if [ ! -f "bin/dropbear" ]; then (
     # dropbear
-    git clone --depth=1 https://github.com/mkj/dropbear.git
-    cd dropbear
+    cd lib/dropbear
     # patch to allow custom user shells
     sed -i -e "s/setusershell()/goto goodshell/g" svr-auth.c
     # build
@@ -75,7 +68,7 @@ if [ ! -f "bin/dropbear" ]; then (
     make -j8
     make -j8 scp
     # copy binaries
-    cp -at ../bin dropbear dropbearkey scp
+    cp -at ../../bin dropbear dropbearkey scp
 ) fi
 
 echo
